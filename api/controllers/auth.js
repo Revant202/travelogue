05a3614470912c6aa8ae7a1738a7a069ae3c7301
Revant <revant202@gmail.com>
2023-01-07 10:54:07 +0530

    added verify token,user and admin

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -1,16 +1,50 @@
-import User from "../models/User.js"
+import User from "../models/User.js";
+import bcrypt from "bcryptjs";
+import { createError } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
-export const register = async(req,res,next)=>{
-    try {
-        const newUser = new User({
-            //not using req.body coz we will encode the password
-            username:req.body.username,
-            email:req.body.email,
-            password:req.body.password,
-        })
-        await newUser.save()
-        res.status(200).send("new user has been created")
-    } catch (error) {
-        next(error)
-    }
-} 
+export const register = async (req, res, next) => {
+  try {
+    const salt = bcrypt.genSaltSync(10);
+    const hash = bcrypt.hashSync(req.body.password, salt);
+
+    const newUser = new User({
+      //not using req.body coz we will encode the password
+      username: req.body.username,
+      email: req.body.email,
+      password: hash,
+    });
+    await newUser.save();
+    res.status(200).json("new user has been created");
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const login = async (req, res, next) => {
+  try {
+    const user = await User.findOne({ username: req.body.username });
+    if (!user) return next(createError(404, "user not found!"));
+    const isPasswordCorrect = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+    if (!isPasswordCorrect)
+      return next(createError(404, "Incorrect username or password!"));
+
+    const token = jwt.sign(
+      { id: user._id, isAdmin: user.isAdmin },
+      process.env.JWT
+    );
+    // generate random key by typing in terminal- openssl rand -base64 32
+    // then, hide the key in .env file
+    // now, store this token in a cookie using cookie parser
+    const { password, isAdmin, ...other } = user._doc;
+    res
+      .cookie("access_token", token, { httpOnly: true })
+      .status(200)
+      .json({ ...other });
+  } catch (error) {
+    next(error);
+  }
+};
--- api/routes/users.js
+++ api/routes/users.js
@@ -1,9 +1,34 @@
 import express from "express";
+import {
+  deleteUser,
+  getAllUsers,
+  getUser,
+  updateUser,
+} from "../controllers/user.js";
+import { verifyToken,verifyUser, verifyAdmin } from "../utils/verifyToken.js";
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.send("hello guyz");
+router.get("/checkToken", verifyToken, (req, res, next) => {
+  res.send("hello user, you are logged in");
 });
+router.get("/checkUser/:id", verifyUser, (req, res, next) => {
+  res.send("hello user, you can update/delete the account");
+});
+router.get("/checkAdmin/:id", verifyAdmin, (req, res, next) => {
+  res.send("hello admin");
+});
+
+//update
+router.put("/:id", updateUser);
+
+//delete
+router.delete("/:id", deleteUser);
+
+//get User
+router.get("/:id", getUser);
+
+//get all User
+router.get("/", getAllUsers);
 
 export default router;
